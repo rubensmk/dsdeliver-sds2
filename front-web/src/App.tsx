fc7237c99314278fbaf8f263daacb2ae7b4dd972
rubensmk <rubenskishimoto@gmail.com>
2021-01-06 10:32:40 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <div >
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
